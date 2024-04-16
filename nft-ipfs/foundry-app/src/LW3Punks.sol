78a821eed507b8634a1f9f7501dc4e7d5abe971b
Ariiellus <arielcardenas95@hotmail.com>
2024-04-16 13:22:23 -0600

    NFT App done

--- nft-ipfs/foundry-app/src/LW3Punks.sol
+++ nft-ipfs/foundry-app/src/LW3Punks.sol
@@ -7,11 +7,22 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract LW3Punks is ERC721Enumerable, Ownable {
     using Strings for uint256;
+    /**
+        * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+        * token will be the concatenation of the `baseURI` and the `tokenId`.
+        */
+    string _baseTokenURI;
 
-    string private _baseTokenURI;
+    //  _price is the price of one LW3Punks NFT
     uint256 public _price = 0.01 ether;
+
+    // _paused is used to pause the contract in case of an emergency
     bool public _paused;
+
+    // max number of LW3Punks
     uint256 public maxTokenIds = 10;
+
+    // total number of tokenIds minted
     uint256 public tokenIds;
 
     modifier onlyWhenNotPaused {
@@ -19,12 +30,18 @@ contract LW3Punks is ERC721Enumerable, Ownable {
         _;
     }
 
-    constructor(string memory baseURI) ERC721("LW3Punks", "LW3P") {
+    /**
+        * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+        * name in our case is `LW3Punks` and symbol is `LW3P`.
+        * Constructor for LW3P takes in the baseURI to set _baseTokenURI for the collection.
+        */
+    constructor (string memory baseURI) ERC721("LW3Punks", "LW3P") Ownable(msg.sender) {
         _baseTokenURI = baseURI;
-        // Assume the ownership setup is intended to transfer ownership to the deployer,
-        // which is automatically handled by `Ownable` and does not require `Ownable(msg.sender)`
     }
 
+    /**
+    * @dev mint allows an user to mint 1 NFT per transaction.
+    */
     function mint() public payable onlyWhenNotPaused {
         require(tokenIds < maxTokenIds, "Exceed maximum LW3Punks supply");
         require(msg.value >= _price, "Ether sent is not correct");
@@ -32,27 +49,50 @@ contract LW3Punks is ERC721Enumerable, Ownable {
         _safeMint(msg.sender, tokenIds);
     }
 
+    /**
+    * @dev _baseURI overrides the Openzeppelin's ERC721 implementation which by default
+    * returned an empty string for the baseURI
+    */
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
 
+    /**
+    * @dev tokenURI overrides the Openzeppelin's ERC721 implementation for tokenURI function
+    * This function returns the URI from where we can extract the metadata for a given tokenId
+    */
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        require(ownerOf(tokenId) != address(0), "ERC721Metadata: URI query for nonexistent token");
+
         string memory baseURI = _baseURI();
+        // Here it checks if the length of the baseURI is greater than 0, if it is return the baseURI and attach
+        // the tokenId and `.json` to it so that it knows the location of the metadata json file for a given
+        // tokenId stored on IPFS
+        // If baseURI is empty return an empty string
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
     }
 
+    /**
+    * @dev setPaused makes the contract paused or unpaused
+        */
     function setPaused(bool val) public onlyOwner {
         _paused = val;
     }
 
-    function withdraw() public onlyOwner {
+    /**
+    * @dev withdraw sends all the ether in the contract
+    * to the owner of the contract
+        */
+    function withdraw() public onlyOwner  {
         address _owner = owner();
         uint256 amount = address(this).balance;
-        (bool sent, ) = _owner.call{value: amount}("");
+        (bool sent, ) =  _owner.call{value: amount}("");
         require(sent, "Failed to send Ether");
     }
 
+        // Function to receive Ether. msg.data must be empty
     receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
     fallback() external payable {}
 }
